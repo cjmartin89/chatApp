11f527a69c07117d2bf3c70484a0ca3a0c4c7d97
Chris Martin <cjmartin89@proton.me>
2024-08-27 00:45:22 -0400

    Added App Drawer

---
+++ Flutter Apps/chat_app/lib/components/my_drawer.dart
@@ -0,0 +1,82 @@
+import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/pages/settings_page.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  // logout
+  void logout() {
+    // get auth service
+    final _auth = AuthService();
+
+    _auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // logo
+          Column(
+            children: [
+              DrawerHeader(
+                child: Icon(
+                  Icons.message,
+                  color: Theme.of(context).colorScheme.primary,
+                  size: 40,
+                  )
+              ),
+              // home list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text('Home', selectionColor: Theme.of(context).colorScheme.primary,),
+                  leading: const Icon(Icons.home),
+                  iconColor: Theme.of(context).colorScheme.primary,
+                  onTap: () {
+                    // pop the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              // settings list tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text('Settings', selectionColor: Theme.of(context).colorScheme.primary),
+                  leading: Icon(Icons.settings),
+                  iconColor: Theme.of(context).colorScheme.primary,
+                  onTap: () {
+                    // pop the drawer
+                    Navigator.pop(context);
+
+                    // navigate to settings page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                      );
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          // logout list tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              title: const Text('Logout'),
+              leading: const Icon(Icons.logout),
+              iconColor: Theme.of(context).colorScheme.primary,
+              onTap: logout,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- Flutter Apps/chat_app/lib/pages/home_page.dart
+++ Flutter Apps/chat_app/lib/pages/home_page.dart
@@ -1,27 +1,19 @@
 import 'package:chat_app/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  // logout
-  void logout() {
-    // get auth service
-    final _auth = AuthService();
-
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
-        actions: [
-          // logout button
-          IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
-        ],
+
       ),
+      drawer: MyDrawer(),
     );
   }
 }
